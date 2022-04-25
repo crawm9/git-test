00ce0bbf44d3d19a102ee4a31b28409317edbaa4
crawm <1840998065@qq.com>
2022-04-25 21:06:18 +0800

    hot-fix commit

--- src/main/java/com/crawm/git/GitTest.java
+++ src/main/java/com/crawm/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello idea git");
         System.out.println("hello idea git2");
         System.out.println("hello idea git3");
+        System.out.println("hello idea git4 hot fix");
     }
 }
