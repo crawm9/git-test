c3452f2cd9fdf274c4aed05197b191c940606281
crawm <1840998065@qq.com>
2022-04-25 20:58:25 +0800

    second commit

--- src/main/java/com/crawm/git/GitTest.java
+++ src/main/java/com/crawm/git/GitTest.java
@@ -3,5 +3,6 @@ package com.crawm.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea git");
+        System.out.println("hello idea git2");
     }
 }
