1ffb56a1964bc005f08448d836a6a5a276d2b99e
crawm9 <57084488+crawm9@users.noreply.github.com>
2022-04-25 21:36:18 +0800

    pull-test commit

--- src/main/java/com/crawm/git/GitTest.java
+++ src/main/java/com/crawm/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello idea git4 hot fix");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
